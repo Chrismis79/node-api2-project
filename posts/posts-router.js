a1ce8c94e1dc49f4606433a7174f3426886f069c
Chrismis79 <chrismis79@gmail.com>
2019-12-03 16:15:11 -0600

    able to add new post

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -18,6 +18,22 @@ router.get('/', (req, res) => {
 });
 
 //POST /api/posts
+router.post('/', (req, res) => {
+    const postData = req.body;
+    if(!postData.title || !postData.contents){
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post."})
+    }else {
+    Posts.insert(postData)
+        .then(post => {
+            res.status(201).json(postData);
+        })
+        .catch(error => {
+            console.log("POST error /api/posts", error);
+            res.status(500)
+                .json({ error: "There was an error while saving the post to the database"})
+        });
+    }
+});
 
 //POST /api/posts/:id/comments
 
