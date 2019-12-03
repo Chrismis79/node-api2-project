ecc994f0938b83d1a8d449418673eef93b9e6870
Chrismis79 <chrismis79@gmail.com>
2019-12-03 17:08:41 -0600

    getting list of posts now working

--- api/server.js
+++ api/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const postsRouter = require('../posts/posts-router');
+const Posts = require('../data/db');
 
 const server = express();
 
 server.use(express.json());
 
 server.get('/api/posts', (req, res) => {
-    res.send(`
-    <h1>Christine's Blog Posts API</h1>`);
+    Posts.find()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(error => {
+        console.log("Error on GET /api/posts from db", error);
+        res.status(500).json({error: "The posts information could not be retrieved."})
+    })
 });
 
 server.use('/api/posts', postsRouter);
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -38,6 +38,22 @@ router.post('/', (req, res) => {
 //POST /api/posts/:id/comments
 
 //GET /api/posts/:id
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Posts.findById(id)
+        .then(post => {
+            if(post) {
+                res.status(200).json(post);
+            }else{
+                res.status(404).json({ message: "The post with the specified ID does not exist."})
+            }
+        })
+        .catch(error => {
+            console.log("Error on GET api/posts/:id", error);
+            res.status(500).json({error: "The post information could not be retrieved."})
+        })
+    
+})
 
 //GET /api/posts/:id/comments
 
